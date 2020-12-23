a2fa937a74b260c61a23b75e9a7d2c9b207c4604
Soumya Dey <soumyadey200@hotmail.com>
2020-12-23 22:30:47 +0530

    minor fixes

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 const app = require("express")();
 const axios = require("axios");
-const queryString = require("query-string");
+const cors = require("cors");
 
 const env = require("./env");
 
-app.get("/", (req, res) => res.send("Hello from localhost:3000"));
+app.use(cors());
 
-app.get("/login/google", (req, res) => {
-  const stringifiedParams = queryString.stringify({
-    client_id: env.CLIENT_ID,
-    redirect_uri: "http://localhost:3000/auth/google",
-    scope: [
-      "https://www.googleapis.com/auth/userinfo.email",
-      "https://www.googleapis.com/auth/userinfo.profile",
-    ].join(" "), // space seperated string
-    response_type: "code",
-    access_type: "offline",
-    prompt: "consent",
-  });
+app.get("/", (req, res) => res.send("Hello from localhost:5000"));
 
-  const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
-
-  res.redirect(googleLoginUrl);
-});
-
-app.get("/auth/google", async (req, res) => {
+app.get("/user/signin", async (req, res) => {
   try {
     const { code } = req.query;
 
@@ -67,12 +51,10 @@ app.get("/auth/google", async (req, res) => {
 
     res.json(userInfo);
   } catch (error) {
-    res
-      .status(500)
-      .json({ errors: [{ msg: `Server error: ${error.message}` }] });
+    res.status(500).json({ errors: [{ msg: `Server error: ${error}` }] });
   }
 });
 
-app.listen(3000, () =>
-  console.log("server started at => http://localhost:3000/")
+app.listen(5000, () =>
+  console.log("server started at => http://localhost:5000/")
 );
