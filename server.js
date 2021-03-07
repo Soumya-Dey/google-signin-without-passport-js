c2f0e88fa5ddd30cee6497a6bdfbafdc5965ba56
Soumya Dey <soumyadey200@hotmail.com>
2021-03-07 21:41:12 +0530

    changes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const app = require('express')();
+const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
 const env = require('./env');
 
+const app = express();
+
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello from localhost:5000'));
